c936dddf0781d51311327c6ba82d776776daaf00
fisica78jfar-ai <fisica78jfar@gmail.com>
2025-10-31 15:23:52 -0600

    1 alcohol

--- script.js
+++ script.js
@@ -25,8 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
         { name: "Perfume mujer(3)", price: 150, img: "img/other perfume women.png" },
         { name: "Perfumitos hombre(3)", price: 60, img: "img/other perfumitos men.png" },
         { name: "Perfumitos mujer(4)", price: 60, img: "img/other perfumitos women.png" },
-
-        { name: "Alcohol verde(2)", price: 80, img: "img/other alcohol.png" },
+        { name: "Alcohol verde(1)", price: 80, img: "img/other alcohol.png" },
         { name: "Cuadernos 80 hojas(10)", price: 30, img: "img/other notebook.png" },
         { name: "Cuadernos 100 hojas(5)", price: 40, img: "img/other notebook 2.png" },
         { name: "Resistol liquido(3)", price: 20, img: "img/other glue.png" },
